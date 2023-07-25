7666b0f9b34ce8aa3c09df4f5fa3dab0faf2fac6
Cesar A. Roman <130246064+Cromananimation@users.noreply.github.com>
2023-07-25 08:19:31 -0500

    Added start script

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const router = require('./notes');
 
 const app = express();
-const port = 5500;
+const port = 8080;
 
 app.use('/feedback', feedbackRouter);
 app.use(express.urlencoded({ extended: false }));
